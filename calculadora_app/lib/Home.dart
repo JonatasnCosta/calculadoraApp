44156988840c9b7820cc3b453fe58df4a284b01a
JonatasCosta1983 <contactworkbetter@gmail.com>
2020-03-28 17:26:20 -0300

    Icones da Appbar integrado

--- calculadora_app/lib/Home.dart
+++ calculadora_app/lib/Home.dart
@@ -8,6 +8,28 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          SizedBox(height: 15.0),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+                  FloatingActionButton(
+                    onPressed: () {},
+                    backgroundColor: Colors.grey.withOpacity(0.3),
+                    mini: true,
+                    elevation: 0.0,
+                    child: Icon(Icons.shopping_cart,
+                        color: Colors.black, size: 17.0),
+                  )
+                ]),
+          )
+        ],
+      ),
+    );
   }
 }
