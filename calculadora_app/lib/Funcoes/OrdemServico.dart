6a8c1eed312d023757e3160195c84aa0c000f649
JonatasCosta1983 <contactworkbetter@gmail.com>
2020-04-04 11:24:16 -0300

    Ponto de  restauração O.S

--- calculadora_app/lib/Funcoes/OrdemServico.dart
+++ calculadora_app/lib/Funcoes/OrdemServico.dart
@@ -1,4 +1,8 @@
+
 import 'package:flutter/material.dart';
+
+
+
 
 
 class OrdemServico extends StatefulWidget {
@@ -6,39 +10,407 @@ class OrdemServico extends StatefulWidget {
   _OrdemServicoState createState() => _OrdemServicoState();
 }
 
+
 class _OrdemServicoState extends State<OrdemServico> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(
-            height: MediaQuery.of(context).size.height,
-            color: Color(0xff399d63),
+  
+      return Scaffold(
+        body: ListView(
+          children: <Widget>[
+            SizedBox(height: 15.0),
+            Container(
+          width: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.only(right: 10.0),
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                //Icones
+                IconButton(
+                  icon: Icon(Icons.arrow_back), 
+                  onPressed: () {}
+                ),
+                FloatingActionButton(
+                  onPressed: () {},
+                  backgroundColor: Colors.grey.withOpacity(0.3),
+                  mini: true,
+                  elevation: 0.0,
+                  child:
+                      Icon(
+                        Icons.short_text, 
+                        color: Colors.black, size: 17.0
+                        ),
+                )
+              ]
+              ),
+        ),
+         Padding(
+          padding: EdgeInsets.all(14.0),
+          child: Text(
+            'Ordem de serviço',
+            style: TextStyle(
+                fontFamily: 'Montserrat',
+                fontSize: 40.0,
+                fontWeight: FontWeight.w500),
           ),
-          Positioned(
-            bottom: 22.0,
+        ),
+         Padding(
+            padding: EdgeInsets.only(top: 10.0, left: 30.0, right: 30.0),
             child: Container(
-            width: MediaQuery.of(context).size.width,
-            child: Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                RotatedBox(quarterTurns: 4, child: Icon(Icons.share, color: Colors.white.withOpacity(0.4)),)
-                ],
+              padding: EdgeInsets.only(left: 45.0),
+              height: 50.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25.0),
+                color: Color(0xff399d63)
+              ),
+              child: Center(
+                child: TextField(
+                   keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Nome da ótica',
+                    hintStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 15.0,
+                      color: Colors.black
+                    ),
+                  ),
+                ),
               ),
             ),
-            )
           ),
-          Container(
-            height: MediaQuery.of(context).size.height - 65.0,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(35.0), bottomRight: Radius.circular(35.0)),
-              color: Colors.white
+          Padding(
+            padding: EdgeInsets.only(top: 10.0, left: 30.0, right: 150.0),
+            child: Container(
+              padding: EdgeInsets.only(left: 45.0),
+              height: 50.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25.0),
+                color: Color(0xff399d63)
+              ),
+              child: Center(
+                child: TextField(
+                   keyboardType: TextInputType.datetime,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Data',
+                    hintStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 15.0,
+                      color: Colors.black
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+         
+           Padding(
+            padding: EdgeInsets.only(top: 10.0, left: 30.0, right: 150.0),
+            child: Container(
+              padding: EdgeInsets.only(left: 45.0),
+              height: 50.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25.0),
+                color: Color(0xff399d63)
+              ),
+              child: Center(
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Número O.S',
+                    hintStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 15.0,
+                      color: Colors.black
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+           Padding(
+            padding: EdgeInsets.only(top: 10.0, left: 30.0, right: 30.0),
+            child: Container(
+              padding: EdgeInsets.only(left: 45.0),
+              height: 50.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25.0),
+                color: Color(0xff399d63)
+              ),
+              child: Center(
+                child: TextField(
+                   keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Tipo de Lente',
+                    hintStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 15.0,
+                      color: Colors.black
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 10.0, left: 30.0, right: 30.0),
+            child: Container(
+              padding: EdgeInsets.only(left: 45.0),
+              height: 50.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25.0),
+                color: Color(0xff399d63)
+              ),
+              child: Center(
+                child: TextField(
+                   keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Tratamento',
+                    hintStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 15.0,
+                      color: Colors.black
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+           Padding(
+            padding: EdgeInsets.only(top: 10.0, left: 30.0, right: 30.0),
+            child: Container(
+              padding: EdgeInsets.only(left: 45.0),
+              height: 50.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25.0),
+                color: Color(0xff399d63)
+              ),
+              child: Center(
+                child: TextField(
+                   keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'OD:     esf     cil      eixo',
+                    hintStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 15.0,
+                      color: Colors.black
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+           Padding(
+            padding: EdgeInsets.only(top: 10.0, left: 30.0, right: 30.0),
+            child: Container(
+              padding: EdgeInsets.only(left: 45.0),
+              height: 50.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25.0),
+                color: Color(0xff399d63)
+              ),
+              child: Center(
+                child: TextField(
+                   keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'OE:     esf     cil      eixo',
+                    hintStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 15.0,
+                      color: Colors.black
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+           Padding(
+            padding: EdgeInsets.only(top: 10.0, left: 30.0, right: 150.0),
+            child: Container(
+              padding: EdgeInsets.only(left: 45.0),
+              height: 50.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25.0),
+                color: Color(0xff399d63)
+              ),
+              child: Center(
+                child: TextField(
+                   keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'ADD',
+                    hintStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 15.0,
+                      color: Colors.black
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 10.0, left: 30.0, right: 150.0),
+            child: Container(
+              padding: EdgeInsets.only(left: 45.0),
+              height: 50.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25.0),
+               color: Color(0xff399d63)
+              ),
+              child: Center(
+                child: TextField(
+                   keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'DNP: OD',
+                    hintStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 15.0,
+                      color: Colors.black
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 10.0, left: 30.0, right: 150.0),
+            child: Container(
+              padding: EdgeInsets.only(left: 45.0),
+              height: 50.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25.0),
+               color: Color(0xff399d63)
+              ),
+              child: Center(
+                child: TextField(
+                   keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'DNP: OE',
+                    hintStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 15.0,
+                      color: Colors.black
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 10.0, left: 30.0, right: 150.0),
+            child: Container(
+              padding: EdgeInsets.only(left: 45.0),
+              height: 50.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25.0),
+                color: Color(0xff399d63)
+              ),
+              child: Center(
+                child: TextField(
+                   keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'ALTURA',
+                    hintStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 15.0,
+                      color: Colors.black
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 10.0, left: 30.0, right: 150.0),
+            child: Container(
+              padding: EdgeInsets.only(left: 45.0),
+              height: 50.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25.0),
+                color: Color(0xff399d63)
+              ),
+              child: Center(
+                child: TextField(
+                   keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Diâmetro',
+                    hintStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 15.0,
+                      color: Colors.black
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 10.0, left: 30.0, right: 150.0),
+            child: Container(
+              padding: EdgeInsets.only(left: 45.0),
+              height: 50.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25.0),
+                color: Color(0xff399d63)
+              ),
+              child: Center(
+                child: TextField(
+                   keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Tipo de aro',
+                    hintStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 15.0,
+                      color: Colors.black
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 10.0, left: 30.0, right: 30.0),
+            child: Container(
+              padding: EdgeInsets.only(left: 45.0),
+              height: 50.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25.0),
+                color: Color(0xff399d63)
+              ),
+              child: Center(
+                child: TextField(
+                   keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Observações',
+                    hintStyle: TextStyle(
+                      fontFamily: 'Montserrat',
+                      fontSize: 15.0,
+                      color: Colors.black
+                    ),
+                  ),
+                ),
+              ),
             ),
           )
-        ],
-      ),
-    );
+],
+),
+);
+
+
   }
 }
+
+
--- calculadora_app/lib/Home.dart
+++ calculadora_app/lib/Home.dart
@@ -21,45 +21,45 @@ with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
    return Scaffold(
-      body: ListView(
-        children: <Widget>[
-          SizedBox(height: 15.0),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            padding: EdgeInsets.only(right: 10.0),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  //Icones
-                  IconButton(
-                    icon: Icon(Icons.menu), 
-                    onPressed: () {}
-                  ),
-                  FloatingActionButton(
-                    onPressed: () {},
-                    backgroundColor: Colors.grey.withOpacity(0.3),
-                    mini: true,
-                    elevation: 0.0,
-                    child:
-                        Icon(
-                          Icons.short_text, 
-                          color: Colors.black, size: 17.0
-                          ),
-                  )
-                ]
+    body: ListView(
+      children: <Widget>[
+        SizedBox(height: 15.0),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.only(right: 10.0),
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                //Icones
+                IconButton(
+                  icon: Icon(Icons.menu), 
+                  onPressed: () {}
                 ),
+                FloatingActionButton(
+                  onPressed: () {},
+                  backgroundColor: Colors.grey.withOpacity(0.3),
+                  mini: true,
+                  elevation: 0.0,
+                  child:
+                      Icon(
+                        Icons.short_text, 
+                        color: Colors.black, size: 17.0
+                        ),
+                )
+              ]
+              ),
+        ),
+        // Título
+        Padding(
+          padding: EdgeInsets.all(14.0),
+          child: Text(
+            'Kit Óptica',
+            style: TextStyle(
+                fontFamily: 'Montserrat',
+                fontSize: 40.0,
+                fontWeight: FontWeight.w500),
           ),
-          // Título
-          Padding(
-            padding: EdgeInsets.all(14.0),
-            child: Text(
-              'Kit Óptica',
-              style: TextStyle(
-                  fontFamily: 'Montserrat',
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.w500),
-            ),
-          ),
+        ),
           //Menu letras
           Padding(
             padding: EdgeInsets.only(left: 15.0),
