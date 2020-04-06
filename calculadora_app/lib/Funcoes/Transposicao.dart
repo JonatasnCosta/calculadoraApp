0a1bca5a3cc12fc790e941ba299d8e4e9ce26d7a
JonatasCosta1983 <contactworkbetter@gmail.com>
2020-04-06 10:59:35 -0300

    Icones e titulo da tela de transposicao adicionado

--- calculadora_app/lib/Funcoes/Transposicao.dart
+++ calculadora_app/lib/Funcoes/Transposicao.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:share/share.dart';
+
 
 
 
@@ -9,81 +9,52 @@ class Transposicao extends StatefulWidget {
 }
 
 class _TransposicaoState extends State<Transposicao> {
-TextEditingController _controllerAlcool = TextEditingController();
-TextEditingController _controllerGasolina = TextEditingController();
 
 
 
-
-  String text = "  _controllerGasolina.text, _controllerAlcool.text";
-  
-
-  
-  @override
+ @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Share Plugin Demo',
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Share Plugin Demo'),
-          ), 
-          body:  Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+           SizedBox(height: 15.0),
+            Container(
+          width: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.only(right: 10.0),
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                TextField(
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                    labelText: 'Share text:',
-                    hintText: 'Enter some text and/or link to share',
-                  ),
-                  controller: _controllerAlcool,
-                
-                 onChanged: (String value) => setState(() {
-                    text = value;
-                  }),
+                //Icones
+                IconButton(
+                  icon: Icon(Icons.arrow_back), 
+                  onPressed: () {}
                 ),
-                TextField(
-                   keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                    labelText: 'Share subject:',
-                    hintText: 'Enter subject to share (optional)',
-                  ),
-                  controller: _controllerGasolina,
-                  
-                  onChanged: (String value) => setState(() {
-                    text = value;
-                  }),
-                ),
-                const Padding(padding: EdgeInsets.only(top: 24.0)),
-               Builder(
-                  builder: (BuildContext context) {
-                    return RaisedButton(
-                      child: const Text('Share'),
-                      onPressed: text.isEmpty
-                          ? null
-                          : () {
-                              // A builder is used to retrieve the context immediately
-                              // surrounding the RaisedButton.
-                              //
-                              // The context's `findRenderObject` returns the first
-                              // RenderObject in its descendent tree when it's not
-                              // a RenderObjectWidget. The RaisedButton's RenderObject
-                              // has its position and size after it's built.
-                              final RenderBox box = context.findRenderObject();
-                              Share.share(text,
-                                    sharePositionOrigin:
-                                      box.localToGlobal(Offset.zero) &
-                                          box.size);
-                            }
-                    );
-                  },
-                ),
-              ],
-            ),
-          )
+                FloatingActionButton(
+                  onPressed: () {},
+                  backgroundColor: Colors.grey.withOpacity(0.3),
+                  mini: true,
+                  elevation: 0.0,
+                  child:
+                      Icon(
+                        Icons.short_text, 
+                        color: Colors.black, size: 17.0
+                        ),
+                )
+              ]
+              ),
+        ),
+         Padding(
+          padding: EdgeInsets.all(14.0),
+          child: Text(
+            'Transposição de lentes',
+            style: TextStyle(
+                fontFamily: 'Montserrat',
+                fontSize: 40.0,
+                fontWeight: FontWeight.w500),
           ),
+        ),
+        ],
+      ),
     );
   }
 }
