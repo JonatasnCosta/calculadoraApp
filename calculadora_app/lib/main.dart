a425a636a531af5686992cd71573995fa5019620
JonatasCosta1983 <contactworkbetter@gmail.com>
2020-05-06 10:16:56 -0300

    provides

--- calculadora_app/lib/main.dart
+++ calculadora_app/lib/main.dart
@@ -19,4 +19,5 @@ void main() {
 //Ponto de integração feito com sucesso da calculadora
 //Ponto de integração para mudar a lógica
 //Ponto de integração para receber conteúdo de transposição
+//Ponto de integração para corrigir erro
 
